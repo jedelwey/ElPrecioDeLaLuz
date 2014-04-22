df67d55ca32a32dfff3ff9aa6a299e26e4affa4e
José David Durán Martos <jedelwey@gmail.com>
2014-04-22 02:34:06 +0200

    Cambios chorras

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -8,23 +8,14 @@ var app = {
     onDeviceReady: function () {
         //Llamamos la función de FastClick para todos los elementos que esten en el body
         FastClick.attach(document.body);
-<<<<<<< HEAD
         $(document).ready(function ()
 		{
-=======
-        $(document).ready(function () {
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
             //prueba 
             $("#tablag").hide();
             $("#tablan").hide();
             $("#tablav").hide();
-<<<<<<< HEAD
 			$("#medianocturna").hide();
             $("#mediageneral").hide();
-=======
-            $("#mediageneral").hide();
-            $("#medianocturna").hide();
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
             $("#mediavehiculo").hide();
 
             //Genero las tablas
@@ -33,10 +24,6 @@ var app = {
             loadGeneral();
             loadNocturna();
             loadVehiculos();
-<<<<<<< HEAD
-			loadNocturna();
-=======
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
             //Creo la entrada por defecto
             $("#oculto").append("general");
             $("#oculto").hide();
@@ -81,12 +68,9 @@ function botonnocturna(){
     error = $("#log").html();
     if(error == "error"){
         $("#error").show();
-<<<<<<< HEAD
         $("#tablaNocturna").hide();
         $("#tablan").hide();
         $("#medianocturna").hide();
-=======
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
     }
 };
 //Pulsar el botón Vehículos
@@ -145,10 +129,6 @@ function mifecha() {
     }
 };
 function loadGeneral() {
-<<<<<<< HEAD
-=======
-//Prueba ajax
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
     $.ajax(
     {
         type        : "GET",
@@ -156,19 +136,11 @@ function loadGeneral() {
         crossDomain : true,
         beforeSend  : function()
         {
-<<<<<<< HEAD
 		$("#log").html("");
 		$("#error").hide();
 		$("#cargando").html("<div id='cargando'><div id='imagencargando'><img src='img/loading.gif'/></div><h1>Cargando...<br>Por favor espere</h1></div>");
 		$("#cargando").show();
 		$("#tablag").hide();
-=======
-            $("#log").html("");
-            $("#error").hide();
-            $("#cargando").html("<div id='cargando'><div id='imagencargando'><img src='img/loading.gif'/></div><h1>Cargando...<br>Por favor espere</h1></div>");
-            $("#cargando").show();
-            $("#tablag").hide();
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
         },
         complete    : function(){
             $("#cargando").html("");
@@ -208,17 +180,13 @@ function loadGeneral() {
             $("#tablag").show();
             $("#mediageneral").show();
             $("#error").hide();
-<<<<<<< HEAD
 			auxa = $("#tablag").html();
-=======
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
         },
         error       : function(){
             $("#error").show();
             $("#log").html("error");
         }
     });
-// FIN AJAX
 };
 function loadNocturna() {
     $.ajax(
@@ -226,7 +194,6 @@ function loadNocturna() {
         type        : "GET",
         url         : "http://www.iniris.es/luz/nocturna",
         crossDomain : true,
-<<<<<<< HEAD
         beforeSend  : function()
         {
 		$("#log").html("");
@@ -239,46 +206,24 @@ function loadNocturna() {
             $("#cargando").html("");
             $("#cargando").hide();
         },                
-=======
-        beforeSend  : function(){
-            $("#log").html("");
-            $("#error").hide();
-            $("#cargando").html("<div id='cargando'><div id='imagencargando'><img src='img/loading.gif'/></div><h1>Cargando...<br>Por favor espere</h1></div>");
-            $("#cargando").show();
-        },
-        complete    : function(){$("#cargando").html("");$("#cargando").hide();},                
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
         dataType    : 'json',
         success     : function(respuestan)
         {
             $.each(respuestan, function (key, valn)
             {
-<<<<<<< HEAD
-                for (var i = 0; i < 50 ; i++) {
-                    alert(key+" "+valn[i]);
-                };
                 for (i = 0; i < 24; i++)
                 {
 
-=======
-                for (i = 0; i < 24; i++) 
-                {
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
                     originalnocturna = valn[i];
                     noct = originalnocturna.substr(2, 6);
                     $("#nocturna" + (i + 1)).html("<div align='center' class='preciohora' id='preciohoran" + noct + "'>Precio intermedio</div><br><div>" + originalnocturna + " &euro; / kWh </div>");
                 };
-<<<<<<< HEAD
                 for (i= 24; i < 25; i++)
                 {
-=======
-                for (i= 24; i < 25; i++){
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
                     originalnocturna = valn[i];
                     $("#medianocturna").html("Fecha: "+fecha+"<br>Precio medio: "+originalnocturna+" &euro;");
                 };
                 reverson = valn.sort();
-<<<<<<< HEAD
                 for (var j = 0; j < 24; j++)
                 {
                     vueltan = reverson[j];
@@ -295,24 +240,12 @@ function loadNocturna() {
                         }
                         else
                         {
-=======
-                for (var j = 0; j < 24; j++) {
-                    vueltan = reverson[j];
-                    noctaux = vueltan.substr(2, 6);
-                    if (j < 8) {
-                        $("#preciohoran"+noctaux).html("Precio más barato");
-                        $("#preciohoran"+noctaux).css("background-color", "green");
-                    } else{
-                        if (j < 16) {
-                        } else{
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
                             $("#preciohoran"+noctaux).html("Precio más caro");
                             $("#preciohoran"+noctaux).css("background-color", "red");
                         };
                     };
                 };
             });
-<<<<<<< HEAD
             $("#tablan").show();
             $("#medianocturna").show();
             $("#error").hide();
@@ -320,18 +253,10 @@ function loadNocturna() {
         },
         error       : function()
         {
-=======
-            $("#medianocturna").show();
-            $("#tablan").show();
-            $("#error").hide();
-        },
-        error       : function(){
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
             $("#error").show();
             $("#log").html("error");
         }
     });
-// FIN AJAX
 };
 //Creo la tabla Vehículos
 function loadVehiculos() {
@@ -346,14 +271,10 @@ function loadVehiculos() {
             $("#cargando").html("<div id='cargando'><div id='imagencargando'><img src='img/loading.gif'/></div><h1>Cargando...<br>Por favor espere</h1></div>");
             $("#cargando").show();
         },
-<<<<<<< HEAD
         complete    : function(){
             $("#cargando").html("");
             $("#cargando").hide();
         },                
-=======
-        complete    : function(){$("#cargando").html("");$("#cargando").hide();},                
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
         dataType    : 'json',
         success     : function(response)
         {
@@ -385,10 +306,7 @@ function loadVehiculos() {
                     };
                 };
             });
-<<<<<<< HEAD
             $("#tablaNocturna").show();
-=======
->>>>>>> f55958bd179ae273b9c66137b0474e112b2498bc
             $("#mediavehiculo").show();
             $("#tablav").show();
             $("#error").hide();
